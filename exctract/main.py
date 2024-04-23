c1c702640c027e416f8e300dbabaa7293bc15fca
Dito Póvoa <dito.povoa@gmail.com>
2024-04-23 10:21:52 -0300

    inclusao de log e de dados extraidos

--- exctract/main.py
+++ exctract/main.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(filename='logs/ingestion.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s'
+                    )
+
 app_api = os.getenv('id_project')
 
 url = "http://ws.audioscrobbler.com/2.0/"
@@ -48,11 +53,17 @@ for metodo in metodos:
 
     if total_pages == actual:
         logging.info("Nao existe mais de uma pagina nesse metodo")
-        data.append(retorno_request)
+        try:
+            name_file = f"data/in/{metodo.split('.')[1]}.json"
+            with open(name_file, 'w') as arquivo:
+                json.dump(retorno_request, arquivo)
+        except Exception as ex:
+            logging.error(f"Falha ao escrever o arquivo JSON {name_file} na pasta do projeto -> {ex}")
 
     else:
         logging.info(f"Foram encontradas {total_pages} de dados nesse metodo, dados da {actual} pagina extraido")
         data.append(retorno_request)
+        
         actual += 1
 
         while actual <= total_pages:
@@ -61,11 +72,23 @@ for metodo in metodos:
                 req = requests.get(url,params=params)
                 retorno_request = json.loads(req.content.decode('utf-8'))
                 data.append(retorno_request)
-                actual += 1
                 logging.info(f"Dados da {actual} pagina extraido")
+                actual += 1
             except Exception as ex:
                 logging.error(f"Falha ao extrair os dados da pagina {actual} em um total de {total_pages} -> Erro: {ex}")
 
+        try:
+            name_file = f"data/in/{metodo.split('.')[1]}.json"
+            with open(name_file, 'a') as arquivo:
+                for position,dado in enumerate(data):
+                    objeto = {"posicao":position, "corpo":dado}
+                    json.dump(objeto, arquivo)
+                    arquivo.write('\n')
+
+        except Exception as ex:
+            logging.error(f"Falha ao escrever o arquivo JSON {name_file} na pasta do projeto -> {ex}")
+
+    
     logging.warning(f"Extraido todos os dados do Metodo {metodo}")
 
 
